bde6d2848c4a31f4a26b422e042315041ea25ea1
codewithkevin <oseikelvin709@gmail.com>
2022-04-23 09:30:23 +0000

    update

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/', (req, res) => {
-    
-    var text = req.body.text
-
-});
-
-
-
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
