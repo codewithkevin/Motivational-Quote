cb47a1f92e5095fa333091060fbf8df66ca298a4
codewithkevin <oseikelvin709@gmail.com>
2022-04-23 09:20:17 +0000

    update

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/signup.html');
+    res.sendFile(__dirname + '/index.html');
 })
 
 app.post('/', (req, res) => {
     
+    var text = req.body.text
+
 });
 
 
