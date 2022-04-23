7c34ad740ef0d4da972d0212620ef45ad843779e
codewithkevin <oseikelvin709@gmail.com>
2022-04-23 11:32:02 +0000

    update

--- public/script/script.js
+++ public/script/script.js
@@ -1,8 +1,12 @@
 //Selectors 
 const text = document.getElementById('text');
+const button = document.getElementById('button');
 
 // Event Listeners
 text.addEventListener('click', display);
 
 
 // Functions
+function display() {
+    text.innerHTML = 'Hello World';
+}
